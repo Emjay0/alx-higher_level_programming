fc941fb9b0ee141857c21f44b2579efcdb6ac872
emjay <adisamujib11@gmail.com>
2022-12-21 01:45:20 -0800

    Task 0 and 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """ Define a class square"""
 
+
 class Square:
-    """ class square that has an attribute named size """
-    
+    """class square that has an attribute named size"""
     def __init__(self, size):
-        """ initialization of the fuction for the class square """
+        """ initialization of the fuction for the class square"""
         self.__size = size
