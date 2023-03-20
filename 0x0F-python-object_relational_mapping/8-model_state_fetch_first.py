1ef9f7bd87f240c5b577efd4ff04f01ae88e7bca
emjay <adisamujib11@gmail.com>
2023-03-20 02:12:37 -0700

    Task 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,26 +1,26 @@
 #!/usr/bin/python3
 """ fetch first states """
-from model_state import Base, State
+
 from sys import argv
-from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-def fetch_states():
-    """ access database print state """
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    state = session.query(State).order_by(State.id).first()
-    if state:
-        print("{}: {}".format(state.id, state.name))
+    instance = session.query(State).order_by(State.id).first()
+
+    if instance is None:
+        print('Nothing')
     else:
-        print("Nothing")
-    session.close()
-
-
-if __name__ == "__main__":
-    fetch_states()
+        print('{0}: {1}'.format(instance.id, instance.name))
